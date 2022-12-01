505d8fe38867b583a23b698dcc0f5c99d475cb8e
LuisDarioHinojosa <darioluishinojosaq@mail.com>
2022-12-01 00:48:46 -0600

    letter2

--- ART_7/canvas/sketch.js
+++ ART_7/canvas/sketch.js
@@ -1,30 +1,44 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
-
 
 // declare the text
 let text = "Q";
 let fontSize = 1200;
-let fontFamily = 'sans';
+let fontFamily = 'Courier New';
 
-const sketch = () => {
+// we need to create another grid
+const typeCanvas = document.createElement("canvas");
+const typeContext = typeCanvas.getContext("2d");
+
+const sketch = ({ context, width, height }) => {
+
+  const cell = 20;
+  const cols = Math.floor(width / cell);
+  const rows = Math.floor(height / cell);
+  const numCells = cols * rows;
+
+  fontSize = cols;
+
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    typeContext.fillStyle = 'black';
+    typeContext.fillRect(0, 0, cols, rows);
 
 
 
     // text parameters  
-    context.fillStyle = 'black';
-    context.font = `${fontSize}px ${fontFamily}`;
-    context.textBaseLine = 'top';
+    typeContext.fillStyle = 'white';
+    typeContext.font = `${fontSize}px ${fontFamily}`;
+    typeContext.textBaseLine = 'top';
     //context.textAlign = 'center';
 
     // measure text
-    const metrics = context.measureText(text);
+    const metrics = typeContext.measureText(text);
     //console.log(metrics);
     // extract the text dimentions to get the bounding box
     const mx = metrics.actualBoundingBoxLeft * -1;
@@ -33,20 +47,55 @@ const sketch = () => {
     const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent
 
     // 
-    const x = (width - mw) * 0.5 -mx;
-    const y = (height - mh) * 0.5 -my;
+    const tx = (cols - mw) * 0.5 -mx;
+    const ty = (rows - mh) * 0.5 -my;
 
     // translate text
-    context.save();
-    context.translate(x,y);
-    context.beginPath();
-    context.rect(mx,my,mw,mh);
-    context.stroke();
-    context.fillText(text,0,0);
-    context.restore();
+    typeContext.save();
+    typeContext.translate(tx,ty);
+    typeContext.beginPath();
+    typeContext.rect(mx,my,mw,mh);
+    typeContext.stroke();
+    typeContext.fillText(text,0,0);
+    typeContext.restore();
 
+    // get rba channels in the data key
+    const typeData = typeContext.getImageData(0,0,cols,rows).data;
+    
+    context.drawImage(typeCanvas,0,0);
+
+    for(let i = 0; i < numCells; i++){
+      const col = i % cols;
+      const row = Math.floor(i/cols);
+      const x = col * cell;
+      const y = row * cell;
+
+      const r = typeData[i * 4 + 0];
+      const g = typeData[i * 4 + 1];
+      const b = typeData[i * 4 + 2];
+      const a = typeData[i * 4 + 3];
+
+      context.fillStyle = `rgb(${r},${g},${b})`
+
+      context.save();
+      context.translate(x,y);
+      context.translate(cell * 0.5, cell * 0.5);
+      //context.fillRect(0,0,cell,cell);
+      context.beginPath();
+      context.arc(0,0,cell * 0.5,0,Math.PI * 2);
+      context.fill();
+      context.restore();
+
+    }
 
   };
 };
+
+// instead of drawing in context we will draw into typeContext
+const onKeyUp = (e) => {
+  console.log(e);
+}
+// listens on the event then the key is released
+document.addEventListener('keyup',onKeyUp);
 
 canvasSketch(sketch, settings);
